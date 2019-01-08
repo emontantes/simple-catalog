3759a798ae4f4c39e1896e29db26c0972c849cf0
Esteban Montantes <esmo.ledev@gmail.com>
2019-01-07 17:59:39 -0700

    faker data populated, added destroy_all before data reloads

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning seeds"
+Product.destroy_all
+puts "seeds are clean!"
+
+puts "sowing seeds and making them grow ^_^-b"
+8.times do
+  Product.create(name: Faker::Commerce.product_name, price: Faker::Commerce.price(range = 0..10.10, as_string = false), description: Faker::Lorem.sentence)
+end
