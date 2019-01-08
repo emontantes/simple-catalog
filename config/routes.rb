cafceb4ea83e9df21f6ebbbe4ba4d3fd805ed046
Esteban Montantes <esmo.ledev@gmail.com>
2019-01-07 18:38:55 -0700

    added title with link to index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'products/index'
   get 'products/show'
 
-  get 'pages/home'
+  root 'pages#home'
   get 'pages/about'
   get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
