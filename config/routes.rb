33f076722a537c0bae6012e84b3e3ae2b3b3d108
Esteban Montantes <esmo.ledev@gmail.com>
2019-01-08 17:49:31 -0700

    jubotron feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'products/index'
 
   root 'pages#home'
-  get 'pages/about'
-  get 'pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
